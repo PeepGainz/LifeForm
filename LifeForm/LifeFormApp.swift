9aba4d625e0b98c6b89ec2eb4dab93b141f4ed78
Ricky Lawrence <97762779+PeepGainz@users.noreply.github.com>
2022-01-17 12:11:10 -0500

    Initial Commit

---
+++ LifeForm/LifeFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeFormApp.swift
+//  LifeForm
+//
+//  Created by Ricky Lawrence on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LifeFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
